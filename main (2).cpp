df13983f1f571a8f267a1d1fe2e2c82814e7ac4b
Pirlo <d.andrea@students.amikom.ac.id>
2026-06-18 19:20:44 +0700

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -118,9 +118,20 @@ void menuRegPetugas(){
 
 //registrasi form warga
 void regWarga(){
-    string rusername, rpassword, runame, rpasswd;
+    int jumlahData = 0;
+    const  int MAX_ARRAY = 100;
+    string rusername, rpassword;
+    string arrRUsername[MAX_ARRAY], arrRPassword[MAX_ARRAY];
+    bool username;
     system("clear");
     cin.ignore();
+
+    ifstream input("fWarga.txt");
+    if(input.is_open()){
+        while(jumlahData < MAX_ARRAY && input arrRUsername[jumlahData] >> arrRPassword[jumlahData]){
+            jumlahData++;
+        }
+    }
     cout << ">>> REGISTRASI FORM SEBAGAI WARGA <<<" << endl;
     cout << "Masukkan Username dan Password" << endl << endl;
     cout << "Username : "; getline(cin, rusername);
@@ -136,9 +147,23 @@ void regWarga(){
 
 //registrasi form petugas
 void regPetugas(){
-    string rusername, rpassword, runame, rpasswd;
-    system("clear");
+    int jumlahData = 0;
+    const int MAX_ARRAY = 100;
+    string rusername, rpassword;
+    string arrRUsername[MAX_ARRAY], arrRPassword[MAX_ARRAY];
+    bool username;
     cin.ignore();
+    system("clear");
+    
+    ifstream input("fPetugas.txt");
+    if(input.is_open()){
+        while (jumlahData < MAX_ARRAY && input >> arrRUsername[jumlahData] >> arrRPassword[jumlahData]){
+            jumlahData++;
+        }
+        input.close();
+    }
+
+
     cout << ">>> REGISTRASI FORM SEBAGAI PETUGAS PEMBERSIHAN <<<" << endl;
     cout << "Masukkan Username dan Password" << endl << endl;
     cout << "Username : "; getline(cin, rusername);
@@ -154,23 +179,36 @@ void regPetugas(){
 
 //login form warga
 void loginWarga(){
-    int isi;
-    string username, password, uname, passwd;
+    int baris = 0;
+    int jumlahData = 0;
+    const int MAX_ARRAY = 100;
+    string username, password;
+    string arrUsername[MAX_ARRAY], arrPassword[MAX_ARRAY];
     system("clear");
     cin.ignore();
+
     cout << ">>> LOGIN FORM SEBAGAI WARGA <<<" << endl;
     cout << "Masukkan Username dan Password" << endl << endl;
     cout << "Username : "; getline(cin, username);
     cout << "Password : "; getline(cin, password);
     ifstream input("fWarga.txt");
 
-    while(input >> uname >> passwd){
-        if(uname == username && passwd == password){
-            isi = 1;
-            system("clear");
-        }
+    while(jumlahData < MAX_ARRAY && input >> arrUsername[jumlahData] >> arrPassword[jumlahData]){
+        // if(uname == username && passwd == password){
+        //     isi = 1;
+        //     system("clear");
+        // }
+        jumlahData++;
     }input.close();
-    if(isi == 1){
+
+    for(int i = 0; i < jumlahData; i++){
+        if(arrUsername[i] == username && arrPassword[i] == password){
+            baris = 1;
+            system("clear");
+            break;
+        }
+    }
+    if(baris == 1){
         cout << username << " berhasil login" << endl;
         MMWarga();
     } else{
@@ -181,23 +219,32 @@ void loginWarga(){
 
 //login form petugas
 void loginPetugas(){
-    int isi;
-    string username, password, uname, passwd;
+    int baris = 0;
+    int jumlahData = 0;
+    const int MAX_ARRAY = 100;
+    string username, password;
+    string arrUsername[MAX_ARRAY], arrPassword[MAX_ARRAY];
     system("clear");
     cin.ignore();
+
     cout << ">>> LOGIN FORM SEBAGAI PETUGAS PEMBERSIHAN <<<" << endl;
     cout << "Masukkan Username dan Password" << endl << endl;
     cout << "Username : "; getline(cin, username);
     cout << "Password : "; getline(cin, password);
     ifstream input("fPetugas.txt");
 
-    while(input >> uname >> passwd){
-        if(uname == username && passwd == password){
-            isi = 1;
-            system("clear");
-        }
+    while(jumlahData < MAX_ARRAY && input >> arrUsername[jumlahData] >> arrPassword[jumlahData]){
+        jumlahData++;
     }input.close();
-    if(isi == 1){
+
+    for (int i = 0; i < jumlahData; i++){
+        if(arrUsername[i] == username && arrPassword[i] == password){
+            baris = 1;
+            system("clear");
+            break;
+        }
+    }
+    if(baris == 1){
         cout << username << " berhasil login" << endl;
         MMPetugas();
     } else{
@@ -210,18 +257,19 @@ void MMWarga(){
     int opsi;
     do{
     cout << ">>> MENU UTAMA SEBAGAI WARGA <<<" << endl;
-    cout << "1. MENU PETUGAS WARGA" << endl;
-    cout << "2. MENU PETUGAS WARGA" << endl;
-    cout << "3. MENU PETUGAS WARGA" << endl;
+    cout << "1. " << endl;
+    cout << "2. Kembali" << endl;
     cout << "Masukkan pilihan anda: "; cin >> opsi;
     switch(opsi){
-        case 3:
+        case 1:
+        break;
+        case 2:
         break;
         default:
         cout << "Pilihan tidak sesuai !!!" << endl;
         break;
     }
-    }while(opsi != 3);
+    }while(opsi != 2);
 }
 
 //menu utama petugas
@@ -232,6 +280,9 @@ void MMPetugas(){
     cout << "1. MENU PETUGAS PEMBERSIHAN" << endl;
     cout << "2. MENU PETUGAS PEMBERSIHAN" << endl;
     cout << "3. MENU PETUGAS PEMBERSIHAN" << endl;
+    cout << "4. MENU PETUGAS PEMBERSIHAN" << endl;
+    cout << "5. MENU PETUGAS PEMBERSIHAN" << endl;
+    cout << "6. MENU PETUGAS PEMBERSIHAN" << endl;
     cout << "Masukkan pilihan anda: "; cin >> opsi;
     switch(opsi){
         case 3:
